b041dedc77661ac97bdcc8066e78a572e47e905b
Cristopher Pérez <cristopher.perez@epn.edu.ec>
2019-10-21 21:20:12 -0500

    Pruebas segunda parte :(

--- mi carpeta/Dart/Prueba_variables.dart
+++ mi carpeta/Dart/Prueba_variables.dart
@@ -1,3 +1,9 @@
 var nombre = "Cristopher";
 const IVA = 12;
-var String = [213,"hola mundo"]
+var String = [213,"hola mundo"];
+console.log(nombre.runtimeType);
+console.log(IVA.runtimeType);
+
+const numero = 10;
+console.log(numero.runtimeType);
+
