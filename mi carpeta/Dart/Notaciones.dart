d9650c49df0b24a105ffe432cfa980e6ce0efa0f
Cristopher Pérez <cristopher.perez@epn.edu.ec>
2019-10-30 20:05:27 -0500

    Deber operaciones Dart

---
+++ mi carpeta/Dart/Notaciones.dart
@@ -0,0 +1,11 @@
+/*
+CONDICIONALES
+if
+else
+
+NOTACION
+Dart utiliza la notacion camelCase, es decir
+ejm: eduardoPerez
+     estoyCasado
+
+*/
---
+++ mi carpeta/Dart/Operaciones_ritmeticas.dart
@@ -0,0 +1,25 @@
+/*
+OPERACIONES 
+
+Suma -> +
+Resta -> -
+Negacion -> -expr
+Mutiplicacion -> *
+Division -> /
+Divide y retorna resultado entero -> ~/
+Modulo -> %
+
+OPERADORES DE IGUALDAD Y RELACIONES
+es igual -> ==
+no es igual -> !=
+mayor que >
+menor que <
+mayor o igual que >=
+menor o igual que <=
+
+OPERADORES LOGICOS
+invierte la expresion -> !expr
+or logico -> ||
+and logico -> &&
+
+ */
