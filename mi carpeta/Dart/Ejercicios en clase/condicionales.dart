2d82d03edf48d6b6c34058ee39dab528efce9340
Cristopher Pérez <cristopher.perez@epn.edu.ec>
2019-11-12 22:12:20 -0500

    un poquito más de dart :(

---
+++ mi carpeta/Dart/Ejercicios en clase/condicionales.dart
@@ -0,0 +1,33 @@
+
+
+void main(){
+ const miEdad = 19;
+ if (miEdad >= 18){
+   print('Soy mayor de Edad');
+ } else {
+   print('Soy menor de Edad');
+ }
+
+
+dynamic deberes = 0;
+deberes = deberes + 1;
+//readLine() ('ingrese la operacion que desea hacer');
+do 
+switch 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
