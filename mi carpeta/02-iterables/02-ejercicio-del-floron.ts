7d9e9692b3809cb342ef37fed9b46a7a35187be4
Cristopher Pérez <cristopher240871@gmail.com>
2019-11-20 17:51:02 -0500

    Ejercicio Floron y alcance de las variables

--- mi carpeta/02-iterables/02-ejercicio-del-floron.ts
+++ mi carpeta/02-iterables/02-ejercicio-del-floron.ts
@@ -1,5 +1,4 @@
 // 1) crear un arreglo de 5 elementos
-const arregloss = [];
 // 2) cada elemento va a ser un 1 o un 0
 // ej: [0, 0, 1, 1, 0]
 // 3) exista al menos un elemento "1"
@@ -24,7 +23,9 @@ const indiceQuintoElemento = 4;
 
 for (const arreglo = []; arreglo.push(Math.floor(Math.random() * 2)) <= numeroDeJugadores;){
     if (arreglo[indicePrimerElemento] == 0 && arreglo[indiceSegundoElemento] == 0 && arreglo[indiceTercerElemento] == 0 && arreglo[indiceCuartoElemento] == 0 && arreglo[indiceQuintoElemento] == 0 ){
-        console.log('Intente de nuevo');
+        for (const arreglo = []; arreglo.push(Math.floor(Math.random() * 2)) <= numeroDeJugadores;){
+            console.log(arreglo);
+        }
     }else{
             console.log(arreglo);
         }
@@ -42,3 +43,32 @@ arreglo[indiceSegundoElemento];
 arreglo[indiceTercerElemento];
 console.log(arreglo[indicePrimerElemento]);
 */
+
+
+
+/*
+1) crear un erreglo de 5 elementos
+2) cada elemento del arreglo debe ser un numero aleatorio entre 0 y 1
+3) si todos los elementos del arreglo son 0, el arreglo no sirve
+4) redefinir un nuevo arreglo
+
+PSEUDOCODIGO DEL PROFE
+
+1) Definir el numero de jugadores
+2) Asignar randomicamente si tiene o no un floron
+3) Veriicar si al menos existe un floron
+4) Adivinar donde esta el floron
+
+*/
+
+const numberOfPlayers = 5;
+const firstPlayerIndex = 0;
+const secondPlayerIndex = 1;
+const thirdPlayerIndex = 2;
+const fourthPlayerIndex = 3;
+const fifthPlayerIndex = 4;
+const cualquierIndice = firstPlayerIndex || secondPlayerIndex || thirdPlayerIndex || fourthPlayerIndex || fifthPlayerIndex;
+
+for (const array = []; arreglo.push(Math.floor(Math.random() * arreglo[first])))
+
+
