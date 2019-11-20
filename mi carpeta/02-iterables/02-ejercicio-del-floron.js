38454017d0f04f623ed716052e98d6a9dd381e1f
Cristopher Pérez <cristopher.perez@epn.edu.ec>
2019-11-20 00:19:09 -0500

    Resuelto el problema del floron????

--- mi carpeta/02-iterables/02-ejercicio-del-floron.js
+++ mi carpeta/02-iterables/02-ejercicio-del-floron.js
@@ -8,6 +8,26 @@ var arregloss = [];
 Math.floor(Math.random() * 2); // -> genera un numero random de 0 a 1 // [0 , 2[
 Math.floor(Math.random() * 11); // -> genera un numero random de 0 a 10 // [0 , 11[
 var numeroDeJugadores = 5;
+var indicePrimerElemento = 0;
+var indiceSegundoElemento = 1;
+var indiceTercerElemento = 2;
+var indiceCuartoElemento = 3;
+var indiceQuintoElemento = 4;
 for (var arreglo = []; arreglo.push(Math.floor(Math.random() * 2)) <= numeroDeJugadores;) {
-    console.log(arreglo);
+    if (arreglo[indicePrimerElemento] == 0 && arreglo[indiceSegundoElemento] == 0 && arreglo[indiceTercerElemento] == 0 && arreglo[indiceCuartoElemento] == 0 && arreglo[indiceQuintoElemento] == 0) {
+        console.log('Intente de nuevo');
+    }
+    else {
+        console.log(arreglo);
+    }
 }
+/*
+const arreglo = [200, 2, 3];
+const indicePrimerElemento = 0;
+const indiceSegundoElemento = 1;
+const indiceTercerElemento = 2;
+arreglo[indicePrimerElemento];
+arreglo[indiceSegundoElemento];
+arreglo[indiceTercerElemento];
+console.log(arreglo[indicePrimerElemento]);
+*/ 
