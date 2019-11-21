712aabffa3bd14826dc9c08087749ed8be3f8a1f
Cristopher Pérez <cristopher.perez@epn.edu.ec>
2019-11-21 16:59:28 -0500

    Funciones y arreglos

---
+++ mi carpeta/03-funciones/01-introduccion.js
@@ -0,0 +1,9 @@
+function log(cualquierCosa // parametro
+    ) {
+    console.log(cualquierCosa);
+}
+log("Hola mundo");
+function laPoli() {
+    console.log("No hay el texto");
+}
+laPoli();
---
+++ mi carpeta/03-funciones/01-introduccion.ts
@@ -0,0 +1,20 @@
+function log(
+    cualquierCosa // parametro
+    ){
+    console.log(cualquierCosa);
+}
+log("Hola mundo");
+
+function noHayEltexto(){
+    console.log("No hay el texto");
+}
+noHayEltexto();
+
+function sumarDosNumeros(
+    a:number,
+    b:number
+    ): number{
+    return a + b;
+}
+
+sumarDosNumeros(4, 2);
